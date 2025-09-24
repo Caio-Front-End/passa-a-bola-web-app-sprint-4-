3481f9f03d54a916b82fcd857e116151f2f17f61
Lucas Cavalcante <lu.cavalcante.jd@gmail.com>
2025-09-24 02:10:08 -0300

    style(ui): utiliza variavel global de cor

--- src/components/SeletorAdmin.jsx
+++ src/components/SeletorAdmin.jsx
@@ -4,12 +4,12 @@ const SeletorAdmin = () => {
   const [selected, setSelected] = useState("jogadora");
   return (
     <>
-    <div className="flex bg-gray-800 rounded-xl w-fit p-1 mt-5 place-self-center gap-1">
+    <div className="flex bg-[var(--bg-color2)] rounded-xl w-fit p-1 mt-5 place-self-center gap-1">
       <button
         className={`px-10 py-3  rounded-xl font-medium focus:outline-none transition-colors duration-300 text-white ${
           selected === "jogadora"
-            ? "bg-[#b554b5] hover:bg-[#d44b84]"
-            : "bg-gray-800 hover:bg-gray-700"
+            ? "bg-[#b554b5] hover:bg-[var(--primary-color-hover)]"
+            : "bg-[var(--bg-color2)] hover:bg-[var(--bg-color2-hover)]"
         }`}
         type="button"
         onClick={() => setSelected("jogadora")}
@@ -19,8 +19,8 @@ const SeletorAdmin = () => {
       <button
         className={`px-10 py-3 rounded-xl font-medium focus:outline-none transition-colors duration-300 text-white ${
           selected === "organizador"
-            ? "bg-[#b554b5] hover:bg-[#d44b84] "
-            : "bg-gray-800 hover:bg-gray-700"
+            ? "bg-[#b554b5] hover:bg-[var(--primary-color-hover)] "
+            : "bg-[var(--bg-color2)] hover:bg-[var(--bg-color2-hover)]"
         }`}
         type="button"
         onClick={() => setSelected("organizador")}
