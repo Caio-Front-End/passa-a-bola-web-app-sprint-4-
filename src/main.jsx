4b2ff00e8fefeb9c7f1277adab69c7dd3231c7c8
Caio Nascimento Battista <nascimento_caio_@hotmail.com>
2025-10-08 00:57:16 -0300

    feat(core): Integra ToastProvider no ponto de entrada da aplicação

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
+// src/main.jsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider } from '../src/contexts/AuthContexts.jsx';
+import { ToastProvider } from './contexts/ToastContext.jsx'; // 1. IMPORTAR O PROVIDER
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ToastProvider>
+          <App />
+        </ToastProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
